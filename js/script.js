4cb55e420c04ee1d6425c6914128333156fe67c7
chujingxl <chujingxl@gmail.com>
2022-06-09 17:42:36 +0800

    完成了portfolio各个项目的说明

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("view-project-btn")) {
     togglePortfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0, 0);
+    portfolioItemDetails(e.target.parentElement);
   }
 })
 
@@ -12,5 +14,19 @@ function togglePortfolioPopup() {
 }
 
 document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup)
+
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("pp-inner")) {
+    togglePortfolioPopup();
+  }
+})
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+  document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+
+}
 
 //end of Portfolio Item Details Popup
