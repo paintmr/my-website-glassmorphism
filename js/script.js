94a1026afd614545b212adf218a70229f11bcc38
chujingxl <chujingxl@gmail.com>
2022-06-13 23:54:30 +0800

    navbar的显示和隐藏，Overlay，Background circles，responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+// sart of Toggle Navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+})
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+// end of Toggle Navbar
+
+// start of Active Section
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    // activate the overlay to prevent multiple clicks
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    const hash = e.target.hash;
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document.querySelector("section.active").classList.remove("active", "fade-out");
+      document.querySelector(hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+})
+// end of Active Section
+
 //start of Portfolio Item Details Popup
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("view-project-btn")) {
